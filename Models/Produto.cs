46a385d11795150f3d23df298dee763d6b5a84ae
Cleiton <cleitoncardososp@gmail.com>
2021-10-28 15:36:43 -0300

    Iniciando Projeto

---
+++ Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O campo quantidade é obrigatório")]
+        [Range(1, 100, ErrorMessage = "Valor não permitido.")]
+        public int Quantidade { get; set; }
+
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
